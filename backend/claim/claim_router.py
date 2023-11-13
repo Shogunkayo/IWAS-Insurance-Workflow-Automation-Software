163f54ccfe3ea1a66b6e8f1a45da5fc8be4e377f
Shogunkayo <pyrotourface@gmail.com>
2023-11-14 01:23:38 +0530

    frontend, claim init

---
+++ backend/claim/claim_router.py
@@ -0,0 +1,8 @@
+class ClaimRouter:
+    def __init__(self, router_id, appraiser_id, verifier_id, adjuster_id) -> None:
+        self.router_id = router_id
+        self.requests = []
+        self.appraiser_id = []
+        self.verifier_id = []
+        self.ajuster_id = []
+        
